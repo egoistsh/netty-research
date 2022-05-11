752ea9a2273714d2bd781c8b379bca18f6daf940
billy <g_billy@163.com>
2022-05-11 21:57:55 +0800

    设置参数

--- src/main/java/io/netty/example/study/server/Server.java
+++ src/main/java/io/netty/example/study/server/Server.java
@@ -4,7 +4,9 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.example.study.server.codec.OrderFrameDecoder;
@@ -14,14 +16,24 @@ import io.netty.example.study.server.codec.OrderProtocolEncoder;
 import io.netty.example.study.server.handler.OrderServerProcessHandler;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
 
 public class Server {
     public static void main(String[] args) {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.channel(NioServerSocketChannel.class);
         serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
-        NioEventLoopGroup group = new NioEventLoopGroup();
-        serverBootstrap.group(group);
+        //最大的等待连接数。默认采取linux中取到的值。
+        serverBootstrap.option(NioChannelOption.SO_BACKLOG, 1024);
+        //启用 Nagle 算法：用将小的碎片数据连接成更大的报文来提高发送效率。
+        serverBootstrap.childOption(NioChannelOption.TCP_NODELAY, true);
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(0, new DefaultThreadFactory("boss"));
+        NioEventLoopGroup workGroup = new NioEventLoopGroup(0, new DefaultThreadFactory("worker"));
+        serverBootstrap.group(bossGroup, workGroup);
+
+        LoggingHandler infoLogHandler = new LoggingHandler(LogLevel.INFO);
+
         serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) throws Exception {
@@ -31,8 +43,7 @@ public class Server {
 
                 pipeline.addLast(new OrderProtocolEncoder());
                 pipeline.addLast(new OrderProtocolDecoder());
-
-                pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+                pipeline.addLast("debuglog", infoLogHandler);
 
                 pipeline.addLast(new OrderServerProcessHandler());
             }
@@ -45,7 +56,8 @@ public class Server {
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            group.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
         }
     }
 }
